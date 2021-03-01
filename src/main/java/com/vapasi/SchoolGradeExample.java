0988922ecb3031f025432d263caca9e55c9e2bf1
shaina <shainanagpal2007@gmail.com>
2021-03-02 03:04:34 +0530

    commiting for assignment-data types

--- src/main/java/com/vapasi/SchoolGradeExample.java
+++ src/main/java/com/vapasi/SchoolGradeExample.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class SchoolGradeExample {
     public static void main(String[] args) {
+        //Checkstudentgrade();
+        Checkstudentattendence();
+    }
+    public static void Checkstudentgrade()
+    {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter your marks");
         double marks=sc.nextDouble();
@@ -11,7 +16,7 @@ public class SchoolGradeExample {
         {
             System.out.println("Grade A");
         }
-       else if(marks>=60 && marks<80)
+        else if(marks>=60 && marks<80)
         {
             System.out.print("Grade B");
         }
@@ -29,5 +34,25 @@ public class SchoolGradeExample {
         }
         else
             System.out.println("Grade F");
+    }
+    public static void Checkstudentattendence()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Number of classes held");
+        int totalclasses=sc.nextInt();
+        System.out.println("Number of classes attended");
+        int attendedclasses=sc.nextInt();
+       if(totalclasses>attendedclasses || totalclasses==attendedclasses) {
+            double attperc = (attendedclasses*100)/totalclasses;
+            System.out.println(attperc);
+            if (attperc >= 75)
+                System.out.println("student can attend the exam");
+            else
+                System.out.println("not eligible for exam");
+       }
+       else
+           System.out.println("incorrect number of classes");
+
+
     }
 }
