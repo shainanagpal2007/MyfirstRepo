6f058858eb9bd1844daf9e363a0ce5c1bd986428
shaina <shainanagpal2007@gmail.com>
2021-03-14 23:33:46 +0530

    Updated code

--- src/main/java/com/vapasi/array/CarNumberArray.java
+++ src/main/java/com/vapasi/array/CarNumberArray.java
@@ -1,20 +1,32 @@
 package com.vapasi.array;
 
+import org.hamcrest.core.IsNull;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class CarNumberArray {
-    public static void main(String[] args) {
+    public static <Bool> void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter total car numbers to be generated");
         int totalnum=sc.nextInt();
         //Generate random numbers
         Random rnum=new Random();
         String[] arr = new String[totalnum];
+        boolean[] isExist=new boolean[10000];
         for(int i=0;i<totalnum;i++)
         {
-            arr[i] = ("MH"+rnum.nextInt(10000));
+            int num =rnum.nextInt(10000);
+            if(!isExist[num])
+            {
+                arr[i] = ("MH" + num);
+                isExist[num]=true;
+            }
+            else
+            {
+                i--;
+            }
         }
         System.out.println("Car Numbers Generated = "+ Arrays.toString(arr));
     }
